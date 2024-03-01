c9b378ea3de8f6ba9dde8d0b73fde14390343de5
stega <stegallj4@gmail.com>
2024-03-01 05:44:42 -0500

    Merge conflict duplication adjustments

--- orders.js
+++ orders.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Orders = [
     {
         productName: 'JavaScript Tutorial',
@@ -18,25 +17,4 @@ const Orders = [
         paymentStatus: 'Paid',
         status: 'Active'
     }
-=======
-const Orders = [
-    {
-        productName: 'JavaScript Tutorial',
-        productNumber: '85473',
-        paymentStatus: 'Due',
-        status: 'Pending'
-    },
-    {
-        productName: 'CSS Full Course',
-        productNumber: '97245',
-        paymentStatus: 'Refunded',
-        status: 'Declined'
-    },
-    {
-        productName: 'FlexBox Tutorial',
-        productNumber: '36452',
-        paymentStatus: 'Paid',
-        status: 'Active'
-    }
->>>>>>> fae1a13e5891e9f34a4427783d344cea682913e3
 ]
